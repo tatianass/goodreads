f665ad30aaf437dfb66e51863231b90abc769a20
tatianass <tatiana.silva@ccc.ufcg.edu.br>
2017-04-10 02:55:37 -0300

    ff project example

---
+++ examples/ff-project.py
@@ -0,0 +1,34 @@
+from goodreads import client
+import json
+
+def singleArray(inputlist):
+    outputlist = []
+    for i in inputlist:
+      outputlist.extend(i)
+    return outputlist
+
+def writeToJSONFile(path, fileName, data):
+    filePathNameWExt = './' + path + '/' + fileName + '.json'
+    with open(filePathNameWExt, 'w') as fp:
+        json.dump(data, fp)
+
+#authetification
+gc = client.GoodreadsClient('fCUHaHylHEkSW3jJcZJTQ', 'dOKwd2Kewa5sIpoAynUEVl44s5wscrwWoAiGwBuijTI')
+
+#get all books by query
+books = gc.search_books_all_pages('Lesbian', search_field='genre')
+
+#parse books to single array
+books = singleArray(books)
+
+#to json
+data = []
+for book in books:
+    i = {}
+    i['title'] = book.title
+    i['author'] = str(book.authors[0])
+    data.append(i)
+    
+writeToJSONFile('./','ff-books',data)
+
+
